39505521ea8c39ec9fecf8d6f25780688f91bd00
mitchelecenteno-lgu <mitchele.centeno@lgusuite.com>
2023-10-20 10:28:28 +0800

    fix snackbar

--- src/app/shared/dialog/contact-dialog/contact-dialog.component.ts
+++ src/app/shared/dialog/contact-dialog/contact-dialog.component.ts
@@ -6,6 +6,8 @@ import {
 } from '@angular/material/dialog';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
+import { SuccessSbComponent } from '../../snackbar/success-sb/success-sb.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-contact-dialog',
@@ -24,8 +26,18 @@ export class ContactDialogComponent {
       Validators.minLength(0),
     ]),
   });
+  durationInSeconds = 3.5;
+  message = 'message sent successfully';
+  action = 'OK';
 
-  // onSubmit(){}
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialogRef: MatDialogRef<ContactDialogComponent>,
+    private snackbar: MatSnackBar
+  ) {}
+
+  ngOnInit() {}
+
   public sendEmail() {
     emailjs
       .send(
@@ -37,6 +49,7 @@ export class ContactDialogComponent {
       .then(
         (result: EmailJSResponseStatus) => {
           console.log('SUCCESS: ', result.text);
+          this.openSnackBar();
         },
         (error) => {
           console.log('ERROR: ', error.text);
@@ -45,15 +58,11 @@ export class ContactDialogComponent {
     this.dialogRef.close();
   }
 
-  constructor(
-    private dialogRef: MatDialogRef<ContactDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
-  ) {}
-
-  ngOnInit() {}
-
-  // onSubmit() {}
-
+  openSnackBar() {
+    this.snackbar.open(this.message, this.action, {
+      duration: this.durationInSeconds * 1000,
+    });
+  }
   onCancel(): void {
     this.dialogRef.close();
   }
