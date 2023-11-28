50d139515c69ca3be3ae5fb71b825e9867f01d8c
mitchelecenteno-lgu <mitchele.centeno@lgusuite.com>
2023-11-28 17:54:01 +0800

    biodata logo animation

---
+++ src/app/interface/descriptions/biodata-interface.ts
@@ -0,0 +1,11 @@
+export interface BioDataEduc {
+  year?: string;
+  course: string;
+  school?: string;
+}
+
+export interface BioDataExp {
+  year?: string;
+  role: string;
+  company?: string;
+}
